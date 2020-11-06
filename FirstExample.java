bab5b4185b9b410b40ac3af2d6c280b920d547ba
Naveen Singh <naveen09cool@gmail.com>
2020-11-06 11:55:52 +0530

    Added 2 function calls

--- FirstExample.java
+++ FirstExample.java
@@ -1,6 +1,10 @@
 class FirstExample{
     public static void main(String[] args) {
-        System.out.println("Hello, World");
-        
+        System.out.println("Hello");
+        System.out.println("Calling the even Numbers func");
+        SecondExample.printEveNumbers(11)
+
+        System.out.println("Calling the Odd Numbers func");
+        ThirdExample.printOddNumbers(11)
     }
 }
