e578ef3ce4b131ca1b7261e212b300b276d2c2d6
Naveen Singh <naveen09cool@gmail.com>
2020-11-06 10:58:32 +0530

    Multiple files Commited

---
+++ SecondExample.java
@@ -0,0 +1,7 @@
+class SecondExample{
+    public static void printEvenNumber(int no){
+        System.out.println("Even number upto "+ no);
+        for(int x=0; x<no; x=x+2)
+            System.out.println(x);
+    }
+}
---
+++ ThirdExample.java
@@ -0,0 +1,7 @@
+class ThirdExample{
+    public static void printOddNumber(int n){
+        System.out.println("Odd number upto "+ n);
+        for(int x=1; x<n; x=x+2)
+            System.out.println(x);
+    }
+}
